ed3ed0f5a0ac39a360ccec015a7694f00b15a4f4
Emre Karayaman <107217989+karayamanemre@users.noreply.github.com>
2023-02-24 00:18:37 +0300

    Add validation specs to User model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,49 @@
+RSpec.describe User, type: :model do
+  describe 'associations' do
+    it 'has many reservations' do
+      expect(described_class.reflect_on_association(:reservations).macro).to eq :has_many
+    end
+
+    it 'has many motorcycles' do
+      expect(described_class.reflect_on_association(:motorcycles).macro).to eq :has_many
+    end
+  end
+
+  it 'validates presence of name' do
+    user = build(:user, name: nil)
+    expect(user).not_to be_valid
+  end
+
+  it 'validates length of name' do
+    user = build(:user, name: 'A')
+    expect(user).not_to be_valid
+
+    user = build(:user, name: 'A' * 26)
+    expect(user).not_to be_valid
+
+    user = build(:user, name: 'John Doe')
+    expect(user).to be_valid
+  end
+
+  it 'validates presence of user_name' do
+    user = build(:user, user_name: nil)
+    expect(user).not_to be_valid
+  end
+
+  it 'validates length of user_name' do
+    user = build(:user, user_name: 'A' * 2)
+    expect(user).not_to be_valid
+
+    user = build(:user, user_name: 'A' * 51)
+    expect(user).not_to be_valid
+
+    user = build(:user, user_name: 'johndoe')
+    expect(user).to be_valid
+  end
+
+  it 'validates uniqueness of user_name' do
+    create(:user, user_name: 'johndoe')
+    user = build(:user, user_name: 'johndoe')
+    expect(user).not_to be_valid
+  end
+end
