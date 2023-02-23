dd09a6b2376684f18891591d7929f347622f3505
natanimabesha <natanimabesha@www.zhewutech.com>
2023-02-23 14:02:20 +0300

    User Model and Validations

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  has_many :reservations, foreign_key: 'user_id', dependent: :destroy
+  has_many :motorcycles, foreign_key: 'user_id', dependent: :destroy
+
+  validates :name, presence: true, length: { minimum: 2, maximum: 25 }
+  validates :user_name, presence: true, length: { minimum: 4, maximum: 50 }, uniqueness: { case_sensitive: false }
+end
