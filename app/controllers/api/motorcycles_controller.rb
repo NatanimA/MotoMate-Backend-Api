8b5867017ca96932ce99c3f2f46722a66b4ecded
Emre Karayaman <107217989+karayamanemre@users.noreply.github.com>
2023-02-23 15:52:32 +0300

    motorcycle controller actions created

--- app/controllers/api/motorcycles_controller.rb
+++ app/controllers/api/motorcycles_controller.rb
@@ -1,2 +1,44 @@
 class Api::MotorcyclesController < ApplicationController
+  before_action :set_motorcycle, only: [:show, :update, :destroy]
+
+  def index
+    @motorcycles = Motorcycle.all
+    render json: @motorcycles
+  end
+
+  def show
+    render json: @motorcycle
+  end
+
+  def create
+    @motorcycle = Motorcycle.new(motorcycle_params)
+
+    if @motorcycle.save
+      render json: @motorcycle, status: :created, location: @motorcycle
+    else
+      render json: @motorcycle.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @motorcycle.update(motorcycle_params)
+      render json: @motorcycle
+    else
+      render json: @motorcycle.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @motorcycle.destroy
+    head :no_content
+  end
+
+  private
+    def set_motorcycle
+      @motorcycle = Motorcycle.find(params[:id])
+    end
+
+    def motorcycle_params
+      params.require(:motorcycle).permit(:name, :img_url, :price, :description, :model_year, :engine, :fuel_type)
+    end
 end
