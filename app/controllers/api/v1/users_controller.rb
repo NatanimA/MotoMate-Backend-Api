0eec5bd770610b97d4ece2c0956942d35ade4403
natanimabesha <natanimabesha@www.zhewutech.com>
2023-02-23 16:06:12 +0300

    Linters Fix

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,28 +1,28 @@
 class Api::V1::UsersController < ApplicationController
-  #CSRF Token Validations SKIP
+  # CSRF Token Validations SKIP
   protect_from_forgery with: :null_session
   def login
-    #Check if user is available
-    user = User.find_by(user_name:params[:username])
-    if not user.nil?
-      render status:200, json:{message:"Login success",status:200,data:user}
+    # Check if user is available
+    user = User.find_by(user_name: params[:username])
+    if user.nil?
+      render status: 403, json: { message: 'Login failure,User does not exist.', status: 403 }
     else
-      render status:403, json:{message:"Login failure,User does not exist.",status:403}
+      render status: 200, json: { message: 'Login success', status: 200, data: user }
     end
   end
 
   def register
-    user = User.new(name:params[:name],user_name:params[:username])
-    #Check if body is valid
+    user = User.new(name: params[:name], user_name: params[:username])
+    # Check if body is valid
     if user.valid?
-      #If valid save the new User
+      # If valid save the new User
       if user.save
-        render status:201, json:{message:"User is registered successfuly.",data:user,status:201}
+        render status: 201, json: { message: 'User is registered successfuly.', data: user, status: 201 }
       else
-        render status:500, json:{message:"Something went wrong please try again later.",status:500}
+        render status: 500, json: { message: 'Something went wrong please try again later.', status: 500 }
       end
     else
-      render status:400, json:{message:"Bad request.",status:400}
+      render status: 400, json: { message: 'Bad request.', status: 400 }
     end
   end
 end
