dd25e7f8c1145ca92078fa60016facaa017391aa
natanimabesha <natanimabesha@www.zhewutech.com>
2023-02-23 14:52:17 +0300

    Insert Required Changes

--- app/models/motorcycle.rb
+++ app/models/motorcycle.rb
@@ -1,8 +1,12 @@
 class Motorcycle < ApplicationRecord
-  belongs_to :user, class_name: 'User'
+  has_many :users, through: :reservations, foreign_key: 'motorcycle_id'
   has_many :reservations, foreign_key: 'motorcycle_id'
 
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
   validates :img_url, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :description, presence: true, length: { minimum: 5 }
+  validates :model_year, presence: true
+  validates :engine, presence: true
+  validates :fuel_type, presence: true
 end
