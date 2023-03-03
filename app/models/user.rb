13ba499be24a4fcc42d5ac79cb997475dc7b3730
Emre Karayaman <107217989+karayamanemre@users.noreply.github.com>
2023-03-03 16:27:15 +0300

    changed user model
    user model changed to only have a name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,4 @@ class User < ApplicationRecord
   has_many :motorcycles, through: :reservations
 
   validates :name, presence: true, length: { minimum: 2, maximum: 25 }
-  validates :user_name, presence: true, length: { minimum: 2, maximum: 50 }, uniqueness: { case_sensitive: false }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -40,10 +40,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_23_084706) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "user_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_name"], name: "index_users_on_user_name", unique: true
   end
 
   add_foreign_key "reservations", "motorcycles"
