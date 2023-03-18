9feaa0b6a10c60b109fbb9b20aaa9a6bb3bc8dea
karayamanemre <107217989+karayamanemre@users.noreply.github.com>
2023-03-18 18:53:33 +0300

    Revert "image upload added"
    This reverts commit e7cb708f7a0cd8e8a8fb6abf3bc2029f3e9e0816.

--- db/migrate/20230223084634_create_motorcycles.rb
+++ db/migrate/20230223084634_create_motorcycles.rb
@@ -3,6 +3,7 @@ class CreateMotorcycles < ActiveRecord::Migration[7.0]
     create_table :motorcycles do |t|
       t.string :name
       t.text :description
+      t.string :img_url
       t.string :model_year
       t.decimal :price
       t.string :engine
